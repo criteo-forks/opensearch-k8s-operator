65fade0e55460f437bd5d9ffcf71b1e23e72aeca
Peter Goron <p.goron@criteo.com>
2024-06-26 08:35:48 +0200

    limit number of replicas to 2 for .opendistro_security index
    by default security-admin.sh deploy .opendistro_security index in auto-expand mode
    on all nodes of the cluster. This approach is not compatible with rackawarenesss.
    We limit this index to two replicas (+ primary).

--- opensearch-operator/pkg/reconcilers/securityconfig.go
+++ opensearch-operator/pkg/reconcilers/securityconfig.go
@@ -46,28 +46,16 @@ do
 echo 'Waiting to connect to the cluster'; sleep 120;
 done;`
 
-	// CRITEO - Disable auto-expand replica because it doesn't work with rackawareness in the current version (using --disable-replica-autoexpand)
-	//          Add 2 replicas (+ primary, so RF3) (using --update_settings 3)
-	ApplyReplicaConfigurationCmdTmpl = `count=0;
-until $ADMIN -ts /usr/share/opensearch/jdk/lib/security/cacerts -cert %s -key %s -cd %s -icl -nhnv --update_settings 2 -h %s -p %v || (( count++ >= 20 ));
-do
-sleep 20;
-done;`
-
-	ApplyDisableAutoExpandConfigurationCmdTmpl = `count=0;
-until $ADMIN -ts /usr/share/opensearch/jdk/lib/security/cacerts -cert %s -key %s -cd %s -icl -nhnv --disable-replica-autoexpand -h %s -p %v || (( count++ >= 20 ));
-do
-sleep 20;
-done;`
-
+	// CRITEO - Add --explicit-replicas 0-2 to force .opendistro_security index to be created with auto-expand mode enabled, up to 2 replicas
 	ApplyAllYmlCmdTmpl = `count=0;
-until $ADMIN -ts /usr/share/opensearch/jdk/lib/security/cacerts -cert %s -key %s -cd %s -icl -nhnv -h %s -p %v || (( count++ >= 20 ));
+until $ADMIN -ts /usr/share/opensearch/jdk/lib/security/cacerts -cert %s -key %s -cd %s --explicit-replicas 0-2 -icl -nhnv -h %s -p %v || (( count++ >= 20 ));
 do
 sleep 20;
 done;`
 
+	// CRITEO - Add --explicit-replicas 0-2 to force .opendistro_security index to be created with auto-expand mode enabled, up to 2 replicas
 	ApplySingleYmlCmdTmpl = `count=0;
-until $ADMIN -ts /usr/share/opensearch/jdk/lib/security/cacerts -cert %s -key %s -f %s -t %s -icl -nhnv -h %s -p %v || (( count++ >= 20 ));
+until $ADMIN -ts /usr/share/opensearch/jdk/lib/security/cacerts -cert %s -key %s -f %s -t %s --explicit-replicas 0-2 -icl -nhnv -h %s -p %v || (( count++ >= 20 ));
 do
 sleep 20;
 done;`
@@ -196,9 +184,7 @@ func (r *SecurityconfigReconciler) Reconcile() (ctrl.Result, error) {
 		clusterHostName := BuildClusterSvcHostName(r.instance)
 		httpPort, securityconfigPath := helpers.VersionCheck(r.instance)
 		cmdArg = fmt.Sprintf(SecurityAdminBaseCmdTmpl, clusterHostName, httpPort) +
-			fmt.Sprintf(ApplyAllYmlCmdTmpl, adminCert, adminKey, securityconfigPath, clusterHostName, httpPort) +
-			fmt.Sprintf(ApplyDisableAutoExpandConfigurationCmdTmpl, adminCert, adminKey, securityconfigPath, clusterHostName, httpPort) +
-			fmt.Sprintf(ApplyReplicaConfigurationCmdTmpl, adminCert, adminKey, securityconfigPath, clusterHostName, httpPort)
+			fmt.Sprintf(ApplyAllYmlCmdTmpl, adminCert, adminKey, securityconfigPath, clusterHostName, httpPort)
 	}
 
 	r.logger.Info("Starting securityconfig update job")
@@ -228,9 +214,7 @@ func BuildCmdArg(instance *opsterv1.OpenSearchCluster, secret *corev1.Secret, lo
 	clusterHostName := BuildClusterSvcHostName(instance)
 	httpPort, securityconfigPath := helpers.VersionCheck(instance)
 
-	arg := fmt.Sprintf(SecurityAdminBaseCmdTmpl, clusterHostName, httpPort) +
-		fmt.Sprintf(ApplyDisableAutoExpandConfigurationCmdTmpl, adminCert, adminKey, securityconfigPath, clusterHostName, httpPort) +
-		fmt.Sprintf(ApplyReplicaConfigurationCmdTmpl, adminCert, adminKey, securityconfigPath, clusterHostName, httpPort)
+	arg := fmt.Sprintf(SecurityAdminBaseCmdTmpl, clusterHostName, httpPort)
 
 	// Get the list of yml files and sort them
 	// This will ensure commands are always generated in the same order
